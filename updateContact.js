8e2dfb2753441b79bd802e6e9b024ba77d656609
mihail petrov <mr.mihail.petrov@gmail.com>
2016-12-06 21:26:59 +0200

    updated function pseudo

--- updateContact.js
+++ updateContact.js
@@ -4,11 +4,29 @@
 // Describe in high level the steps this function will do.
 
 
-function updateContacts(connectionString, query, validator)
-    check if connection is good
-    check if query is valid
-    for db accesed via connectionString
-        execude the query
-    then if validator is provide, check if validation  passes
-    if ok =>  return the number of raws affected
-    else print error and handle error
+function updateContacts(connectionString, query / data, validator)
+
+check
+if db exists
+if connection is good
+if user requesting is authorized
+
+create Xhrhttp request / ajax request
+
+then check
+if query is valid and request type is Update
+make sure there is a valid url
+
+for db accesed via connectionString
+if validation is needed
+for the data then validate
+
+execute the query or send the data.
+
+if aplicable check validation passes
+if ok, then
+return the proper response code number and
+return the result to the request as per the protocol
+
+if needed perfom additional actions like console.log / logger log / send notifs / report number of rows affected
+if error print error and handle error
