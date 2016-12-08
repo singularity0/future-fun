e833c5906b958912625f02a3f86e01e06054903b
mihail petrov <mr.mihail.petrov@gmail.com>
2016-12-08 16:51:43 +0200

    quick resolion needs refactoring and bug fixing

--- Games/readJson.js
+++ Games/readJson.js
@@ -12,25 +12,73 @@ function loadJSON(callback) {
     xobj.send(null);
 }
 
-function getLoadedJson() {
+function init() {
     loadJSON(function(response) {
-        // Parse JSON string into object
+
         var actual_JSON = JSON.parse(response);
 
-        getGameName(actual_JSON);
-        //console.log(onClickParams(game = actual_JSON['featured'][0]).gameMachineID)
+        $("#featured").on('click', function(featured) {
+            var featured = actual_JSON['featured']
+            featured.forEach(function(item) {
+                var itemDetails = onClickParams(item);
+                populate(itemDetails);
+            })
+        })
+
+        $("#slots").on('click', function(featured) {
+            var featured = actual_JSON['slots']
+            featured.forEach(function(item) {
+                var itemDetails = onClickParams(item);
+                populate(itemDetails);
+            })
+        })
+
+        $("#card").on('click', function(featured) {
+            var featured = actual_JSON['card']
+            featured.forEach(function(item) {
+                var itemDetails = onClickParams(item);
+                populate(itemDetails);
+            })
+        })
+
+        $("#table").on('click', function(featured) {
+            var featured = actual_JSON['table']
+            featured.forEach(function(item) {
+                var itemDetails = onClickParams(item);
+                populate(itemDetails);
+            })
+        })
     })
 }
 
-getLoadedJson();
+function populate(context) {
 
-function getGameName(actual_JSON) {
-    return actual_JSON['featured'].forEach(function(item) {
-        // console.log(item['game_name']);
-    })
+    var gameTemplateScript = $("#game-template").html();
+
+    var gameTemplate = Handlebars.compile(gameTemplateScript);
+
+    var theCompiledHtml = gameTemplate(context);
+    console.log(context.gameName);
+    $(".container").append(theCompiledHtml);
 }
 
-var entry = "Text and some-rndome % dd In thEs"
+function onClickParams(game = actual_JSON['featured'][1]) {
+
+    var gameNameClean = clearUppercaseAndSymbols(game.game_name);
+    var gameUrl = appendResultToUrl(gameNameClean);
+
+
+    return {
+        gameName: game.game_name,
+        gameCode: game.game_code,
+        gameMachineID: game.machine_id,
+        gameDenomination: game.denominations,
+        gameHands: game.hands,
+        gameUrl: gameUrl
+    }
+}
+
+init();
 
 function clearUppercaseAndSymbols(entry) {
 
@@ -43,31 +91,3 @@ function appendResultToUrl(entry, url = 'http://cacheimg.casinomidas.com/images/
     let entireUrl = url + entry + urlending;
     return entireUrl;
 }
-
-// console.log(appendResultToUrl('ronin'));
-
-var onClickParams = function(game = actual_JSON['featured'][0]) {
-
-    // console.log(game.game_name);
-    return {
-        gameName: game.game_name,
-        gameCode: game.game_code,
-        gameMachineID: game.machine_id,
-        gameDenomination: game.denominations,
-        gameHands: game.hands
-    }
-}
-
-function populate(onClickParams) {
-
-    var gameTemplateScript = $("#game-template").html();
-
-    var gameTemplate = Handlebars.compile(gameTemplateScript);
-
-    var theCompiledHtml = gameTemplate(onClickParams.game_name);
-
-    $(".container").html(theCompiledHtml);
-}
-
-$("#featured").on('click', populate)
-console.log('end');
