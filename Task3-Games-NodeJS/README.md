e8e4abb6189768f3c74bc1390cea386fc71fd5e0
mihail petrov <mr.mihail.petrov@gmail.com>
2016-12-11 23:29:35 +0200

    refactored doc ready ev. currelty clicked class working. typo in css addressed

---
+++ Task3-Games-NodeJS/README.md
@@ -0,0 +1,2 @@
+# future-fun
+3 warm up tasks
--- Task3-Games-NodeJS/app/config/constants.js
+++ Task3-Games-NodeJS/app/config/constants.js
@@ -1,4 +1,4 @@
-let port = process.env.port || 1234;
+let port = process.env.PORT || 1234;
 let prodUrl = 'https: //challengesfuture.herokuapp.com';
 
 module.exports = {
